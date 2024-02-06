1e47025e027e77d5ec691335ac4a8b7fc3a6fdb3
Cameron C <cameronehe@gmail.com>
2024-02-06 11:38:26 -0600

    Initial Push

--- Scripts/WFCCell.cs
+++ Scripts/WFCCell.cs
@@ -1,27 +1,27 @@
-﻿namespace hamsterbyte.WFC{
-    using System;
-    using System.Linq;
+namespace hamsterbyte.WFC{
+	using System;
+	using System.Linq;
 
-    public partial class WFCCell{
-        private void PrecalculateFrequencies(){
-            
-        }
+	public partial class WFCCell{
+		private void PrecalculateFrequencies(){
+			
+		}
 
-        public void RemoveOption(int i){
-            
-        }
+		public void RemoveOption(int i){
+			
+		}
 
-        public double Entropy => 0;
-
-
-        private int WeightedRandomIndex(){
-            
-            return -1;
-        }
+		public double Entropy => 0;
 
 
-        public int Collapse(){
-            return -1;
-        }
-    }
+		private int WeightedRandomIndex(){
+			
+			return -1;
+		}
+
+
+		public int Collapse(){
+			return -1;
+		}
+	}
 }
