e17d45532b7e08a4e09baddae949ab3b794e6ccc
Cameron C <cameronehe@gmail.com>
2024-04-04 20:36:28 -0500

    cont

--- Scripts/WFCRegionManager.cs
+++ Scripts/WFCRegionManager.cs
@@ -147,6 +147,7 @@ public class RegionManager {
 
     // send update to the specified region
 		regions[_requestedRegion.X, _requestedRegion.Y].RegionAppendBorCellUpdate(_borCellUpdate);
+		GD.Print($"region ({_requestedRegion.X}, {_requestedRegion.Y}) had cell({_borCellUpdate.X},{_borCellUpdate.Y}) updated with {_borCellUpdate.OptionsRemovedList.Count} options removed");
 		return;
 	}
 
